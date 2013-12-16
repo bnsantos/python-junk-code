275867707728a54d16c1fae2f88dbb84f9ed12ca
bruno <bnsantos17@gmail.com>
2013-12-16 19:09:58 -0200

    partition using rank

--- algorithms/statistics/rank.py
+++ algorithms/statistics/rank.py
@@ -7,3 +7,15 @@ def rank(unsorted_list, value):
         if val <= value:
             pos += 1
     return pos
+
+
+def partition(unsorted_list, value):
+    lesser = []
+    greater = []
+    for val in unsorted_list:
+        if val < value:
+            lesser.append(val)
+        else:
+            greater.append(val)
+    final_list = lesser + [value] + greater
+    return final_list
--- tests/rankTest.py
+++ tests/rankTest.py
@@ -36,3 +36,42 @@ class TestRank(unittest.TestCase):
 
     def test_rank10(self):
         self.assertEqual(6, Rank.rank([99, 88, 77, 66, 44, 55, 33, 22, 11, -13, -15, 18, -98], 22))
+
+    def test_partition1(self):
+        self.assertEqual([31, 45, 51, 28, 33, 11, 27, 36, 66, 91, 66, 82, 89, 84],
+                         Rank.partition([31, 45, 91, 51, 66, 82, 28, 33, 11, 89, 84, 27, 36], 66))
+
+    def test_partition2(self):
+        self.assertEqual([11, 31, 45, 91, 51, 66, 82, 28, 33, 11, 89, 84, 27, 36],
+                         Rank.partition([31, 45, 91, 51, 66, 82, 28, 33, 11, 89, 84, 27, 36], 11))
+
+    def test_partition3(self):
+        self.assertEqual([31, 45, 51, 66, 82, 28, 33, 11, 89, 84, 27, 36, 91, 91],
+                         Rank.partition([31, 45, 91, 51, 66, 82, 28, 33, 11, 89, 84, 27, 36], 91))
+
+    def test_partition4(self):
+        self.assertEqual([11, 31, 45, 91, 51, 66, 82, 28, 33, 11, 89, 84, 27, 36],
+                         Rank.partition([31, 45, 91, 51, 66, 82, 28, 33, 11, 89, 84, 27, 36], 11))
+
+    def test_partition5(self):
+        self.assertEqual([10, 6, 23, 99, 74, 65, 23, 30, 48, 234, 52],
+                         Rank.partition([99, 74, 65, 23, 30, 10, 6, 48, 234, 52], 23))
+
+    def test_partition6(self):
+        self.assertEqual([30, 2, 17, 12, 39, 44, 99, 102, 48, 83, 52, 68, 44],
+                         Rank.partition([99, 102, 48, 83, 52, 30, 2, 17, 68, 12, 44, 39], 44))
+
+    def test_partition7(self):
+        self.assertEqual([12, 32, 23, 33, 34, 54, 43, 33, 56, 48, 87, 99],
+                         Rank.partition([12, 32, 23, 34, 54, 43, 33, 56, 48, 87, 99], 33))
+
+    def test_partition8(self):
+        self.assertEqual([2, 3, 3, 4, 9, 8, 5, 5, 7, 4], Rank.partition([9, 8, 5, 2, 3, 5, 7, 3, 4], 4))
+
+    def test_partition9(self):
+        self.assertEqual([-1, -15, 2, -87, -33, 27, 99, 84, 46, 27],
+                         Rank.partition([-1, -15, 2, 99, 84, -87, -33, 46, 27], 27))
+
+    def test_partition10(self):
+        self.assertEqual([11, -13, -15, 18, -98, 22, 99, 88, 77, 66, 44, 55, 33, 22],
+                         Rank.partition([99, 88, 77, 66, 44, 55, 33, 22, 11, -13, -15, 18, -98], 22))
